6c3fd383cfe4c72c90d467e3f2c636416b139a34
Ralx-hash <apavezp@uft.edu>
2026-02-19 23:52:39 -0300

    Mensaje para usuarios, solo se les permite ver sus datos. Datos de usuario pasa a estar dentro de un dialog

--- src/app/lista-usuarios/lista-usuarios.component.ts
+++ src/app/lista-usuarios/lista-usuarios.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { SeguridadService } from '../seguridad/seguridadService';
 import { UsuarioListaDTO } from '../../models/users';
 import { take } from 'rxjs';
@@ -11,14 +12,30 @@ import { PerfilComponent } from '../compartido/perfil/perfil.component';
 
 @Component({
   selector: 'app-lista-usuarios',
-  imports: [MatToolbarModule, MatButtonModule, CommonModule, MatPaginatorModule, MatTableModule, 
-    PerfilComponent],
+  imports: [MatToolbarModule, MatButtonModule, CommonModule, MatPaginatorModule, MatTableModule, MatDialogModule],
   templateUrl: './lista-usuarios.component.html',
   styleUrl: './lista-usuarios.component.css'
 })
 export class ListaUsuariosComponent implements OnInit{
 
   ngOnInit(): void {
+    this.userServicio.obtenerPerfil().subscribe({
+      next:(data) => {
+        if(data.rol === 'admin' || data.rol === 'supervisor'){
+          this.verificadorUsuario = true;
+          console.log('Usuario tiene permisos de admin/supervisor');
+        }
+        else{
+          this.verificadorUsuario = false;
+          console.log('Usuario no tiene permisos');
+        }
+      },      
+      error: (err) => {
+        console.error('Error al obtener perfil', err);
+      }
+    });
+
+
     this.userServicio.obtenerUsuarios().pipe(take(1)).subscribe({
       next: (data) => {
         this.listaUsuarios = data;
@@ -31,11 +48,19 @@ export class ListaUsuariosComponent implements OnInit{
     
   }
 
+  verificadorUsuario: boolean = false;
+
   listaUsuarios: UsuarioListaDTO[] = [];
   
   userServicio = inject(SeguridadService);
 
   displayedColumns: string[] = ['id', 'nombre', 'rol', 'renta_mensual'];
+
+  dialog = inject(MatDialog);
+
+  abrirDialog() {
+    const dialogRef = this.dialog.open(PerfilComponent);
+  }
 
 
 
